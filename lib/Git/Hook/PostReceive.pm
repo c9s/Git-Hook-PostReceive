ca9851f8ba716f2ed3c36c9f9f123376ec234587
c9s <cornelius.howl@gmail.com>
2012-06-28 15:49:45 +0800

    initial commit

---
+++ lib/Git/Hook/PostReceive.pm
@@ -0,0 +1,32 @@
+package Git::Hook::PostReceive;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Git::Hook::PostReceive -
+
+=head1 SYNOPSIS
+
+  use Git::Hook::PostReceive;
+
+=head1 DESCRIPTION
+
+Git::Hook::PostReceive is
+
+=head1 AUTHOR
+
+Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
