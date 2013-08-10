13fb999d3c1e26640c6f6d85a544dde0ab5ea7e4
Jakob Voss <jakob@nichtich.de>
2013-08-10 11:45:45 +0200

    migrated to Dist::Zilla

--- lib/Git/Hook/PostReceive.pm
+++ lib/Git/Hook/PostReceive.pm
@@ -1,10 +1,11 @@
+use strict;
 package Git::Hook::PostReceive;
+#ABSTRACT: Parses git commit information in post-receive hook scripts
+
 use v5.10;
 use DateTime::Format::DateParse;
 use Cwd;
 use File::Basename;
-
-our $VERSION = '0.01';
 
 sub read_stdin {
     my ($self, $line) = @_;
@@ -20,12 +21,12 @@ sub run {
     my $is_new_head = $before =~ /^0{40}/;
     my $is_delete = $after =~ /^0{40}/;
 
-    $before  = $before ne '0000000000000000000000000000000000000000' 
-                ? qx(git rev-parse $before) 
+    $before  = $before ne '0000000000000000000000000000000000000000'
+                ? qx(git rev-parse $before)
                 : undef;
 
-    $after   = $after ne '0000000000000000000000000000000000000000' 
-                ? qx(git rev-parse $after) 
+    $after   = $after ne '0000000000000000000000000000000000000000'
+                ? qx(git rev-parse $after)
                 : undef;
 
     chomp($before) if $before;
@@ -40,10 +41,10 @@ sub run {
         repository => $repo,
         ref        => $ref_name,
         ref_type   => $ref_type,
-        ( $is_new_head 
+        ( $is_new_head
             ? (new_head => $is_new_head)
             : () ),
-        ( $is_delete 
+        ( $is_delete
             ? (delete => $is_delete)
             : () ),
         commits    => \@commits,
@@ -75,14 +76,14 @@ sub get_commits {
         $buffer .= $_ . "\n";
     }
     push @commits, $buffer;
-    return reverse map { 
+    return reverse map {
                 my @lines = split /\n/,$_;
                 my $info = {  };
                 for my $line ( @lines ) {
                     given($line) {
                         when( m{^commit (.*)$}i ) { $info->{id} = $1; }
-                        when( m{^author:\s+(.*?)\s<(.*?)>}i ) { 
-                                $info->{author} = { 
+                        when( m{^author:\s+(.*?)\s<(.*?)>}i ) {
+                                $info->{author} = {
                                     name => $1,
                                     email => $2
                                 };
@@ -100,9 +101,6 @@ sub get_commits {
 
 1;
 __END__
-=head1 NAME
-
-Git::Hook::PostReceive - 
 
 =head1 SYNOPSIS
 
@@ -110,8 +108,8 @@ Git::Hook::PostReceive -
     use Git::Hook::PostReceive;
     my $payload = Git::Hook::PostReceive->new->read_stdin( <STDIN> );
 
-    $payload->{new_head}; 
-    $payload->{delete}; 
+    $payload->{new_head};
+    $payload->{delete};
 
     $payload->{before};
     $payload->{after};
@@ -129,42 +127,6 @@ Git::Hook::PostReceive -
 
 Git::Hook::PostReceive parses git commit information in post-receive hook script.
 
-all you need to do is pass the stdin string to Git::Hook::PostReceive, 
+all you need to do is pass the STDIN string to Git::Hook::PostReceive,
 then it returns the commit payload .
 
-=head1 INSTALLATION
-
-Git::Hook::PostReceive installation is straightforward. If your CPAN shell is set up,
-you should just be able to do
-
-    % cpan Git::Hook::PostReceive
-
-Download it, unpack it, then build it as per the usual:
-
-    % perl Makefile.PL
-    % make && make test
-
-Then install it:
-
-    % make install
-
-=head1 DOCUMENTATION
-
-Git::Hook::PostReceive documentation is available as in POD. So you can do:
-
-    % perldoc Git::Hook::PostReceive
-
-to read the documentation online with your favorite pager.
-
-=head1 AUTHOR
-
-Yo-An Lin E<lt>cornelius.howl {at} gmail.comE<gt>
-
-=head1 SEE ALSO
-
-=head1 LICENSE
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=cut
